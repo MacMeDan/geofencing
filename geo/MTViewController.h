2a34baa8380a171515b1d801d313da58471c6850
P Leonard <danielleonard@getjive.com>
2014-05-07 17:34:56 -0600

    Initial Commit

---
+++ geo/MTViewController.h
@@ -0,0 +1,13 @@
+//
+//  MTViewController.h
+//  geo
+//
+//  Created by P Leonard on 5/7/14.
+//  Copyright (c) 2014 com.Macme.geofence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTViewController : UIViewController
+
+@end
---
+++ geo/MTViewController.m
@@ -0,0 +1,29 @@
+//
+//  MTViewController.m
+//  geo
+//
+//  Created by P Leonard on 5/7/14.
+//  Copyright (c) 2014 com.Macme.geofence. All rights reserved.
+//
+
+#import "MTViewController.h"
+
+@interface MTViewController ()
+
+@end
+
+@implementation MTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
