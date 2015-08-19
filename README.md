92e55702c3e7562e4a209e8b82343d3af4f09545
Peter Leonard <plen@jive.com>
2015-08-19 10:03:43 -0600

    Updated project to swift and added icon img and more graphical interface.

--- README.md
+++
@@ -1,4 +0,0 @@
-## Geofencing
-
-An application for an ios device that demostrates how to use geofencecs
-Hoping to be able to fire off events based on where I am. 
---
+++ WhereInTheWorldIsCarmenSandiago/Utillities.swift
@@ -0,0 +1,26 @@
+//
+//  Utillities.swift
+//  WhereInTheWorldIsCarmenSandiago
+//
+//  Created by P Leonard on 8/18/15.
+//  Copyright (c) 2015 Jive. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+// MARK: Helper Functions
+
+func showSimpleAlertWithTitle(title: String!, #message: String, #viewController: UIViewController) {
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+    let action = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
+    alert.addAction(action)
+    viewController.presentViewController(alert, animated: true, completion: nil)
+}
+
+func zoomToUserLocationInMapView(mapView: MKMapView) {
+    if let coordinate = mapView.userLocation.location?.coordinate {
+        let region = MKCoordinateRegionMakeWithDistance(coordinate, 10000, 10000)
+        mapView.setRegion(region, animated: true)
+    }
+}
